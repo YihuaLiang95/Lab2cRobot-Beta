965dc312445ecec46cf892b231ecd25d19d15926
Zephyr <ryanwang96@hotmail.com>
2019-04-06 14:24:33 +0800

    :grinning:tf-yolov3

--- Robot-Vision/object-detection/darknet_2c/demo.sh
+++
@@ -1,5 +0,0 @@
-export CUDA_VISIBLE_DEVICES= 1
-# ./darknet detect cfg/yolov3.cfg yolov3.weights data/dog.jpg
-./darknet detector demo cfg/coco.data cfg/yolov3.cfg yolov3.weights test_multiface.mp4
-
-
