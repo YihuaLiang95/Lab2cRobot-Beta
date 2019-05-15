155e2a1ef137f1a653ecf09432e2720953d4ba5d
Zifeng <1399320648@qq.com>
2019-05-15 13:55:22 +0800

    :tada: re add speech module

---
+++ Robot_Speech/config.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+class Config:
+    eng_name_chinese = {
+        "jiehua":"杰华",
+        "jielian":"杰廉",
+        "liangsheng":"良盛",
+        "linzhang":"professor 张林",
+        "luliu":"柳璐",
+        "shaolun":"professor 绍伦",
+        "shiv":"Shiv",
+        "sikai":"思凯",
+        "wangbin":"王斌",
+        "wangpan":"潘旺",
+        "weijia":"唯嘉",
+        "weitao":"伟涛",
+        "xiangxiang":"祥祥",
+        "xinyi":"鑫义",
+        "yida":"义达",
+        "yihua":"艺铧",
+        "yuyang":"宇扬",
+        "zelong":"泽龙",
+        "zifeng":"子丰",
+        "unknown":"我好像不认识你呢",
+    }
+    hello_list = ["Hello,","你好,","牛批,","优秀","你好6啊,六六六,","加油,给自己打个气,"]
+
+config = Config()
+
