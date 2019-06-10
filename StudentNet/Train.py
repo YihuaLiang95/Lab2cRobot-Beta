bf7bfa8607df61fe9c463ecda78c3970a60669dd
LionED <zhangyuyang1996@gmail.com>
2019-06-10 18:42:23 +0800

    change the structure

--- StudentNet/Train.py
+++ StudentNet/Train.py
@@ -1,5 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 18:37:38 2019
+
+@author: zhang
+"""
+
 import time
-import tensorflow.examples.tutorials.mnist.input_data as input_data
 import tensorflow as tf
 import config as cfg
 import os
@@ -7,30 +13,30 @@ import lenet
 from lenet import Lenet
 from PIL import Image
 import numpy as np
-import random
 from sklearn.model_selection import train_test_split
 
-import pdb
+import time
 
 def load_imagenet():
-    root_ = 'D:/下载/imagenet'
+    root_ = './'
     
     data = []
     label_flag = []
     flag = 0
     
     for folder in os.listdir(root_):
-        for image in os.listdir('%s/%s/'%(root_, folder)):
-            img = Image.open('%s/%s/%s'%(root_, folder, image)).resize((224,224))
-            if img.mode != "RGB":
-                img = img.convert("RGB")
-            img_data = np.asarray(img, dtype='f')
-            if len(img_data.shape) == 3:
-                data.append(img_data)
-                # data.append(img_data)
-                label_flag.append(flag)
-        
-        flag += 1
+        if '.' not in folder:
+            for image in os.listdir('%s/%s/'%(root_, folder)):
+                img = Image.open('%s/%s/%s'%(root_, folder, image)).resize((224,224))
+                if img.mode != "RGB":
+                    img = img.convert("RGB")
+                img_data = np.asarray(img, dtype='f')
+                if len(img_data.shape) == 3:
+                    data.append(img_data)
+                    # data.append(img_data)
+                    label_flag.append(flag)
+            
+            flag += 1
     # pdb.set_trace()
     # one hot
     label = np.eye(flag, dtype='f')[label_flag]
@@ -39,65 +45,57 @@ def load_imagenet():
     
 def main():
     # mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-    '''imagenet = load_imagenet()
-    
+    '''
+    imagenet = load_imagenet()
+
     batch_feature, batch_label = imagenet
     np.save('batch_feature.npy', batch_feature)
     np.save('batch_label.npy', batch_label)
     '''
-    batch_feature = np.load('E:/Python3/knowledge_distillation/datasets/batch_feature.npy')
-    batch_label = np.load('E:/Python3/knowledge_distillation/datasets/batch_label.npy')
-    
+    print('script start')
+    batch_feature = np.load('batch_feature.npy', mmap_mode = 'r').astype(np.float32)
+    print('imgae load')
+    batch_label = np.load('batch_label.npy', mmap_mode = 'r').astype(np.float32)
+    print('data label')
     batch_x, x_test, batch_y, y_test =train_test_split(batch_feature,batch_label,test_size=0.3, random_state=0)
     
-    sess = tf.Session()
+    print('Data ready')
+
+    # sess = tf.Session()
     batch_size = cfg.BATCH_SIZE
     parameter_path = cfg.PARAMETER_FILE
     lenet = Lenet()
     max_iter = cfg.MAX_ITER
-    
-    
+    # pdb.set_trace()
+    print('Parameters loaded') 
 
-    saver = tf.train.Saver()
+    saver = tf.train.Saver(max_to_keep=5)
     if os.path.exists(parameter_path):
         saver.restore(parameter_path)
     else:
-        sess.run(tf.global_variables_initializer())
+        lenet.global_variables_initializer()
     
-    # 创建placeholder
-    # batch_x = tf.cast(batch_x, dtype=tf.float32)
-    # print(batch_x.dtype)
-    features_placeholder = tf.placeholder(tf.float32, batch_x.shape)
-    labels_placeholder = tf.placeholder(tf.float32, batch_y.shape)
-    
-    # 创建dataset
-    dataset = tf.data.Dataset.from_tensor_slices((features_placeholder, labels_placeholder))
-    
-    # 批量读取,打散数据,repeat()
-    dataset = dataset.shuffle(50).batch(batch_size).repeat() 
-    iterator = dataset.make_initializable_iterator()
-    
-    sess.run(iterator.initializer, feed_dict={features_placeholder: batch_x,labels_placeholder: batch_y})
-    next_element = iterator.get_next()
+    lenet.iterator_initializer(batch_x, batch_y)
+    print("Dataset Initialized")
 
     for i in range(max_iter):
         #batch = mnist.train.next_batch(50)
-        x_batch, y_batch = sess.run(next_element)
-        if i % 10 == 0:
-            train_accuracy = sess.run(lenet.train_accuracy,feed_dict={
-                lenet.input_images: x_batch,lenet.raw_input_label: y_batch
-            })
-            loss = sess.run(lenet.loss,feed_dict={
-                lenet.input_images: x_batch,lenet.raw_input_label: y_batch
-            })
+        x_batch, y_batch = lenet.get_next()
+         
+        lenet.train_optimal(x_batch, y_batch)
+        # print("Iter {}".format(i))
+        
+        if i % 100 == 0:
+            start_time = time.clock()
+            train_accuracy = lenet.get_accuracy(x_batch, y_batch)
+            loss = lenet.get_loss(x_batch, y_batch)
             print("step %d, training accuracy %.3f" % (i, train_accuracy))
             print("step %d, loss %.3f" % (i, loss))
             
-            test_accuracy = sess.run(lenet.train_accuracy,feed_dict={
-                lenet.input_images: x_test, lenet.raw_input_label: y_test
-            })
+            test_accuracy = lenet.get_accuracy(x_test, y_test)
             print("test accuracy %.3f" % (test_accuracy))
-        sess.run(lenet.train_op,feed_dict={lenet.input_images: x_batch, lenet.raw_input_label: y_batch})
+            print('The time cost of training: %.2f'%(time.clock() - start_time))
+        
         '''try:
             sess.run(lenet.train_op,feed_dict={lenet.input_images: x_batch, lenet.raw_input_label: y_batch})
         except:
@@ -105,14 +103,16 @@ def main():
             pass
            ''' 
 
-    save_path = saver.save(sess, parameter_path)
+    save_path = saver.save(lenet.sess, parameter_path)
+    
+    fps_x_test = x_test[:-100]
+    # fps_y_test = y_test[:-100]
+    
     start_time = time.clock()
-    test_accuracy = sess.run(lenet.train_accuracy,feed_dict={
-                lenet.input_images: x_test, lenet.raw_input_label: y_test
-            })
+    digits = lenet.predict(fps_x_test)
     end_time = time.clock()
-    print("test accuracy %.3f" % (test_accuracy))
     print('Fps : %.3f'%(end_time - start_time))
 
 if __name__ == '__main__':
+    print('*' * 20)
     main()
--- StudentNet/lenet.py
+++ StudentNet/lenet.py
@@ -1,13 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 18:37:39 2019
+
+@author: zhang
+"""
+
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 import config as cfg
 
 class Lenet:
     def __init__(self):
+        self.sess = tf.Session()
         self.input_images = tf.placeholder(tf.float32, [None, 224, 224, 3])
-        self.raw_input_label = tf.placeholder(tf.float32, [None, 3])
-        self.input_labels = tf.cast(self.raw_input_label,tf.int32)
+        self.input_labels = tf.placeholder(tf.float32, [None, 5])
+        
         self.dropout = cfg.KEEP_PROB
+        
+        self.iterator = None
 
         with tf.variable_scope("Lenet") as scope:
             self.train_digits = self.construct_net(True)
@@ -19,23 +29,70 @@ class Lenet:
         self.train_accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, "float"))
 
 
-        self.loss = tf.losses.softmax_cross_entropy( self.input_labels, self.train_digits)
+        self.loss = tf.losses.softmax_cross_entropy( onehot_labels = self.input_labels, logits = self.pred_digits)
         self.lr = cfg.LEARNING_RATE
         self.train_op = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
 
+    def get_loss(self, input_images, input_labels):
+        loss = self.loss
+        feed_dict = {self.input_images : input_images, self.input_labels : input_labels}
+        return self.sess.run(loss, feed_dict = feed_dict)
+    
+    def train_optimal(self, input_images, input_labels):
+        train_op = self.train_op
+        feed_dict = {self.input_images : input_images, self.input_labels : input_labels}
+        return self.sess.run(train_op, feed_dict = feed_dict)
+    
+    def get_accuracy(self, input_images, input_labels):
+        get_accuracy = self.train_accuracy
+        feed_dict = {self.input_images : input_images, self.input_labels : input_labels}
+        return self.sess.run(get_accuracy, feed_dict = feed_dict)
 
+    def predict(self, x_test):
+        feed_dict = {self.input_images:x_test}
+        pred =  self.train_digits
+        return self.sess.run(pred, feed_dict = feed_dict)
+    
+    def iterator_initializer(self, batch_x, batch_y):
+        features_placeholder = tf.placeholder(tf.float32, batch_x.shape)
+        labels_placeholder = tf.placeholder(tf.float32, batch_y.shape)
+
+        dataset = tf.data.Dataset.from_tensor_slices((features_placeholder, labels_placeholder))
+
+        dataset = dataset.shuffle(50).batch(cfg.BATCH_SIZE).repeat()
+        self.iterator = dataset.make_initializable_iterator()
+
+        feed_dict = {features_placeholder : batch_x, labels_placeholder : batch_y}
+        init = self.iterator.initializer
+        self.sess.run(init, feed_dict = feed_dict)
+        
+
+    def get_next(self):
+        next_element = self.iterator.get_next()
+        return self.sess.run(next_element)
+
+    def global_variables_initializer(self):
+
+        init = tf.global_variables_initializer()
+        self.sess.run(init)
 
     def construct_net(self,is_trained = True):
         with slim.arg_scope([slim.conv2d], padding='VALID',
                             weights_initializer=tf.truncated_normal_initializer(stddev=0.01),
                             weights_regularizer=slim.l2_regularizer(0.0005)):
-            net = slim.conv2d(self.input_images,6,[3,3],1,padding='SAME',scope='conv1')
+            net = slim.conv2d(self.input_images,8,[3,3],1,padding='SAME',scope='conv1')
             net = slim.max_pool2d(net, [2, 2], scope='pool2')
             net = slim.conv2d(net,16,[3,3],1,scope='conv3')
             net = slim.max_pool2d(net, [2, 2], scope='pool4')
             net = slim.conv2d(net,32,[3,3],1,scope='conv5')
+            
+            net = slim.conv2d(net,5,[1,1],1,scope="conv6") # [None,k,k,5]
+            net = tf.reduce_mean(net, [1,2], name="global_avg_pooling") # [None, 5]
+            digits = tf.nn.softmax(net) # [None, 5]
+            '''
             net = slim.flatten(net, scope='flat6')
-            net = slim.fully_connected(net, 84, scope='fc7')
+            net = slim.fully_connected(net, 64, scope='fc7')
             net = slim.dropout(net, self.dropout,is_training=is_trained, scope='dropout8')
-            digits = slim.fully_connected(net, 3, scope='fc9')
+            digits = slim.fully_connected(net, 5, scope='fc9')
+            '''
         return digits
